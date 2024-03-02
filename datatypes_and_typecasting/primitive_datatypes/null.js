f0d0649fcb10ee71b88184d5684a1bca376536db
CHEGEBB <chegephil24@gmail.com>
2024-03-02 21:29:31 +0300

    null

---
+++ datatypes_and_typecasting/primitive_datatypes/null.js
@@ -0,0 +1,9 @@
+//the null value is a special value that represents the absence of a value or a non-existent value. It is a primitive value and is not an object. It is a falsy value.
+//what we mean by falsy value is that it is a value that is considered false when encountered in a boolean context.
+//also  its is used when we want to show a variable has nothing in it or is empty
+let age;
+console.log(age);//undefined
+
+let firstName = null;
+console.log(firstName);//null
+console.log(typeof firstName);//object
