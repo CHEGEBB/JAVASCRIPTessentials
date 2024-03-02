12cab957258e7d027191e7f999ec261438537def
CHEGEBB <chegephil24@gmail.com>
2024-03-02 22:57:13 +0300

    number

---
+++ datatypes_and_typecasting/TypeConversions/CONVERSION/conversionToNumber.js
@@ -0,0 +1,5 @@
+let earthFacts = "The Earth is the only planet in our solar system not to be named after a Greek or Roman deity.";
+let earthFactsNumber = Number(earthFacts);
+console.log(earthFactsNumber);
+console.log(typeof earthFactsNumber);
+//do the same for other primitive data types
