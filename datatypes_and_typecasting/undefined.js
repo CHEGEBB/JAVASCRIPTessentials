c33df1f2770777eb83f17633bbafb8fa29aa3727
CHEGEBB <chegephil24@gmail.com>
2024-03-02 21:20:27 +0300

    undefined

---
+++ datatypes_and_typecasting/undefined.js
@@ -0,0 +1,7 @@
+//undefined gives or is the default value given to a variable after declaration
+let weight;
+console.log(typeof weight);//undefined
+
+// we can also assign the undefined value to a variable HOWEVER THIS IS NOT RECOMMENDED
+let height = undefined;
+console.log(typeof height);//undefined
