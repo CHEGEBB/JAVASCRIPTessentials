1799691db8586731e3449ef90ee5ae04a4e13052
CHEGEBB <chegephil24@gmail.com>
2024-03-03 21:44:10 +0300

    update

---
+++ complex_datatypes/array/array.js
@@ -0,0 +1,15 @@
+//an array is used in javascript as a collection of data items
+//it is a collection of data items
+//declaring a simple array
+let languages = ["python", "javascript", "c++", "java", "c#"];
+console.log(languages[3]);
+console.log(languages[0]);
+//changing items in an array 
+languages [2] = "c";
+//adding items in an array
+languages [5] = "Ruby";
+console.log(languages);
+//removing data items from an array
+delete languages[5];
+console.log(languages);
+//to initialize an empty array
