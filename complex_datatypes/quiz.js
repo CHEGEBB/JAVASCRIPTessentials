30e3e2b15a59a44db9c6640cb65f1e13bb36ecf2
CHEGEBB <chegephil24@gmail.com>
2024-03-04 20:35:09 +0300

    update

--- complex_datatypes/quiz.js
+++ complex_datatypes/quiz.js
@@ -4,6 +4,16 @@
     to :"Mombasa",
     price :600,
  };
- console.log(ticketTrian.from);
- console.log(trainTicket.to);
- console.log(trainTicket.price);
+console.log(`Ticket from : ${trainTicket.from}`);
+console.log(`Ticket to : ${trainTicket.to}`);
+console.log(`Ticket price : ${trainTicket.price}`);
+
+//Declare an empty object and save it to a person variable.
+// Using dot notation, add the name and surname fields to the object by entering your data as values.
+// Try to display the individual fields on the console.
+let person = {};
+person.name = "Kelvin";
+person.surname = "Hart";
+console.log(person.name);
+console.log(person.surname);
+console.log(`name:  ${person.name}  surname:  ${person.surname} `);
