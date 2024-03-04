9c977a520cd64226c17196abd6579d065325ef6a
CHEGEBB <chegephil24@gmail.com>
2024-03-04 20:22:29 +0300

    update

---
+++ complex_datatypes/quiz.js
@@ -0,0 +1,9 @@
+//Create an object that describes a train ticket and store it in the ticket variable. The object must have three fields:
+ let trainTicket = {
+    from :"nairobi",
+    to :"Mombasa",
+    price :600,
+ };
+ console.log(ticketTrian.from);
+ console.log(trainTicket.to);
+ console.log(trainTicket.price);
