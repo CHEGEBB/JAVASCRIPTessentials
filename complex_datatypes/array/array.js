7ed9875761180c29ec79d618fbe9a5c6f6035659
CHEGEBB <chegephil24@gmail.com>
2024-03-04 14:31:35 +0300

    update

--- complex_datatypes/array/array.js
+++ complex_datatypes/array/array.js
@@ -18,3 +18,14 @@ let names =new Array("John","Doe","Jane","Doe");
 console.log(names);
 console.log(ages);
 console.log(typeof names);
+console.log(typeof ages);
+//an array can acually contain data items of different data types
+let arr1 =[1,"Hello world",false, 3.14];
+//arrays can also conatin arrays as data items this creates a multi-dimensional array
+let people = [[ "joseph","arimthea","judas","kioko"], ["john","peter","james","andrew"]];
+//so the first array is actually in index 0 and the second one in index 1
+//accessing data items we must give it the index of the array and the index of the data item
+console.log(people [0] [1]);//arimthea
+console.log(people [1] [2]);//james
+console.log(people [0] [3]);//kioko
+console.log(people [1] [4]);//andrew
