49e619aaadf2be739a3fc891ae3db47a99dd33b4
CHEGEBB <chegephil24@gmail.com>
2024-03-05 21:29:50 +0300

    comments

---
+++ comments/comments.js
@@ -0,0 +1,11 @@
+//In javascrit we have a single line comment which is this
+//console.log("This is a single line comment");
+//we also have a multi line comment which is this
+/*
+console.log("This is a multi line comment");
+console.log("This is a multi line comment");
+console.log("This is a multi line comment");
+console.log("This is a multi line comment");
+console.log("This is a multi line comment");
+*/
+///thats all from comments adios amigos :)
