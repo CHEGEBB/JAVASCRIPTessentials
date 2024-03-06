312ebf82358afcfb1da3402b89603aeb9dee5c73
CHEGEBB <chegephil24@gmail.com>
2024-03-06 22:41:13 +0300

    assignment

---
+++ operators/assignmentOperator.js
@@ -0,0 +1,13 @@
+//The assignment operator is used to assign a value to a variable.
+//The assignment operator (=) assigns a value to a variable.
+//example
+let city = "London";
+let number = 7;
+//if multiple assignments appear iin a sequence the right to left ordr is respected
+let year =2023;
+// let newYear = year = 2024;//commented this as let does not let us reassign a variable
+///this is the same as
+year = 2024;
+let newYear = year;
+//thats all for the assignment operator
+// Path: operators/arithmeticOperators.js
