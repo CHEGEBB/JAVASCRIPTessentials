e78085b636834e864ff9349947671d935f2115cd
CHEGEBB <chegephil24@gmail.com>
2024-03-06 23:14:19 +0300

    compound assignment

---
+++ operators/compoundAssignment.js
@@ -0,0 +1,30 @@
+//addition assignment
+let a = 1;
+a += 2;
+//this is like saying a = a + 2
+console.log(a);//3
+//subtraction assignment
+let b = 3;
+b -= 2;
+//this is like saying b = b - 2
+console.log(b);//1
+//multiplication assignment
+let c = 2;
+c *= 3;
+//this is like saying c = c * 3
+console.log(c);//6
+//division assignment
+let d = 6;
+d /= 3;
+//this is like saying d = d / 3
+console.log(d);//2
+//remainder assignment
+let e = 5;
+e %= 2;
+//this is like saying e = e % 2
+console.log(e);//1
+//exponential assignment
+let f = 2;
+f **= 3;
+//this is like saying f = f ** 3
+console.log(f);//8
