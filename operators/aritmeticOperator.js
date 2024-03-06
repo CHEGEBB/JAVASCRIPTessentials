019b33a10cbfc09a4f3ba02e42eb6233a1a8fa05
CHEGEBB <chegephil24@gmail.com>
2024-03-06 22:48:15 +0300

    update

---
+++ operators/aritmeticOperator.js
@@ -0,0 +1,10 @@
+//these opeartors are used to perfirm arithmetic operations on numbers
+//they include addition, subtraction, multiplication, division, and modulus
+//They adhere to the BODMAS rule as in maths
+//parentheses forces precedence
+//1.Addition operator for  addind two numbers together it converts type to number
+let sum = 15 + 25;
+console.log(sum);//40
+//however the addition operator  when used in strings is just used for concatenation
+let greetings = "Hello" + "World";
+console.log(`${typeof greetings} : ${greetings}`);//
