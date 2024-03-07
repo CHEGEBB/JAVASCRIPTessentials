1b1ae0e0a511aad2b2b5700ec590b2d5bd39dd81
CHEGEBB <chegephil24@gmail.com>
2024-03-07 22:54:09 +0300

    update

---
+++ String_operators/concatenation.js
@@ -0,0 +1,10 @@
+//to concatenate means to join together
+//concatenation is the process of joining two strings together
+//concatenation is done using the + operator
+let  sentence = "Happy New  ";
+let  year = "Year";
+let newYear = sentence + year;
+let newYear2 = newYear + 2024;
+console.log(newYear);
+console.log(newYear2);
+console.log(typeof newYear2);
