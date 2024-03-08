6a8875699731b0608821478f8ab649059a79109f
CHEGEBB <chegephil24@gmail.com>
2024-03-08 17:58:15 +0300

    update

--- ControlStatements-loops/if-statement/if.js
+++ ControlStatements-loops/if-statement/if.js
@@ -22,3 +22,17 @@ if(quantity > 0){
 }
 console.log(total);
 //university grading system using if statement
+let unit = prompt("Enter the unit of study to calculate the grade");
+let grade = prompt(`kindly Enter the marks You scored to calculate your grade in ${unit}`);
+if(grade >= 0 && grade < 40 ){
+    alert(`Your scrore in ${unit} is a D`);
+}
+    if (grade >= 40 && grade < 60 ){
+        alert(`Your grade in ${unit} is a C`);
+    }
+    if(grade >= 60 && grade < 70){
+        alert(`Your score in ${unit} is a B`);
+    }
+    if(grade >= 70 && grade < 100){
+        alert(`Eiiish Einstein You scored an A !!! in ${unit}`);
+    }
