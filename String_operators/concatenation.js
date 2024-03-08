7a8badffbb17fff6c435b7d467232e65acf72616
CHEGEBB <chegephil24@gmail.com>
2024-03-08 11:27:18 +0300

    update

--- String_operators/concatenation.js
+++ String_operators/concatenation.js
@@ -3,10 +3,14 @@
 //concatenation is done using the + operator
 let  sentence = "Happy New  ";
 let  year = "Year";
+let str = "123";
+str = +str;
 let newYear = sentence + year;
 let newYear2 = newYear + 2024;
 console.log(newYear);
 console.log(newYear2);
 console.log(`${typeof newYear2} : ${newYear2}`);//string : Happy New Year2024
 let warm = "Hello" + " "+ "Mike" + " ,Welcome to the world of programming";
+
 console.log(warm);
+console.log (`${typeof str} : ${str}`);
