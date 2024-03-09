b68a932eeff9caa860cc4361e2aa4715dee2d0f8
CHEGEBB <chegephil24@gmail.com>
2024-03-09 10:27:17 +0300

    update

--- ControlStatements-loops/LOOPS/whileLoop/dist/while.dev.js
+++ ControlStatements-loops/LOOPS/whileLoop/dist/while.dev.js
@@ -1,3 +1,17 @@
-//The while loop is a loop in javascript that repeats execution of a given block of code so long as the condition is true
-//example:a program tha 
 "use strict";
+
+//The while loop is a loop in javascript that repeats execution of a given block of code so long as the condition is true
+//example:a program that adds all even numbers between 0 and 100 and gives the result
+var sum = 0; // Initialize a variable to hold the sum of even numbers
+
+var number = 1; // Initialize a variable to start from 1
+
+while (number <= 100) {
+  if (number % 2 === 0) {
+    sum += number;
+  }
+
+  number++;
+}
+
+console.log("The sum of even numbers between 1 and 100 is: " + sum);
--- ControlStatements-loops/LOOPS/whileLoop/while.js
+++ ControlStatements-loops/LOOPS/whileLoop/while.js
@@ -1,2 +1,13 @@
 //The while loop is a loop in javascript that repeats execution of a given block of code so long as the condition is true
-//example:a program tha 
+//example:a program that adds all even numbers between 0 and 100 and gives the result
+let sum = 0; // Initialize a variable to hold the sum of even numbers
+let number = 1; // Initialize a variable to start from 1
+
+while (number <= 100) {
+    if (number % 2 === 0) {
+
+        sum += number;
+    }
+    number++;
+}
+console.log("The sum of even numbers between 1 and 100 is: " + sum);
