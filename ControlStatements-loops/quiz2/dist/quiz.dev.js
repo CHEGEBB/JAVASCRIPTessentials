2ebef4d57ec1d1dbe9aac30230993f386951470b
CHEGEBB <chegephil24@gmail.com>
2024-03-09 20:42:41 +0300

    update

--- ControlStatements-loops/quiz2/dist/quiz.dev.js
+++ ControlStatements-loops/quiz2/dist/quiz.dev.js
@@ -4,9 +4,13 @@ for (var i = 100; i >= 0; i -= 10) {
   console.log(i);
 }
 
-var first = parseFloat(prompt("What is the first number?"));
-var last = parseFloat(prompt("What is the last number?"));
+var first = prompt("What is the first number?");
+var last = prompt("What is the last number?");
 
 for (var j = first; j >= last; j -= 10) {
   console.log(j);
 }
+
+var x = 100;
+if (x < 100) x = 20;
+console.log(x);
--- ControlStatements-loops/quiz2/quiz.js
+++ ControlStatements-loops/quiz2/quiz.js
@@ -1,8 +1,13 @@
 for (let i=100; i>=0; i-=10) {
     console.log(i);
 }
-let first = parseFloat(prompt("What is the first number?"));
-let last = parseFloat(prompt("What is the last number?"));
+let first =prompt("What is the first number?");
+let last = prompt("What is the last number?");
 for (let j = first; j>=last;j-=10){
     console.log(j);
 }
+
+let x = 100;
+if (x < 100)
+    x = 20;
+console.log(x)
