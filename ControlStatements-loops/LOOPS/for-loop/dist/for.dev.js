35c80f5659daa1f6547fc482866ec9891044592c
CHEGEBB <chegephil24@gmail.com>
2024-03-09 10:46:43 +0300

    update

--- ControlStatements-loops/LOOPS/for-loop/dist/for.dev.js
+++ ControlStatements-loops/LOOPS/for-loop/dist/for.dev.js
@@ -1,2 +1,36 @@
-//The 
 "use strict";
+
+//The  for loop is used to execute a block of code a number of times. The for loop has the following syntax:
+// for (initialization; condition; increment/decrement) {
+//   // code block to be executed
+// }
+// The initialization statement is executed before the loop starts. It is only executed once.
+// The condition is evaluated at the beginning of every loop iteration and will continue as long as it is true.
+//example 1
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+} //output
+//0
+//example 2 a more complex example
+
+
+var sum = 0;
+
+for (var _i = 0; _i < 5; _i++) {
+  sum += _i;
+}
+
+console.log(sum); //output
+//10
+//example 3
+
+var sum2 = 0;
+
+for (var _i2 = 0; _i2 <= 100; _i2++) {
+  if (_i2 % 2 === 0) {
+    sum2 += _i2;
+  }
+}
+
+console.log("The sum of even numbers between 0 and 100 is: " + sum); //output
+//The sum of even numbers between 0 and 100 is: 2550
--- ControlStatements-loops/LOOPS/for-loop/for.js
+++ ControlStatements-loops/LOOPS/for-loop/for.js
@@ -1,1 +1,32 @@
-//The 
+//The  for loop is used to execute a block of code a number of times. The for loop has the following syntax:
+// for (initialization; condition; increment/decrement) {
+//   // code block to be executed
+// }
+// The initialization statement is executed before the loop starts. It is only executed once.
+// The condition is evaluated at the beginning of every loop iteration and will continue as long as it is true.
+//example 1
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+//output
+//0
+//example 2 a more complex example
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+  sum += i;
+}
+console.log(sum);
+//output
+//10
+//example 3
+let sum2 = 0;
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        sum2 += i;
+    }
+}
+
+console.log("The sum of even numbers between 0 and 100 is: " + sum);
+//output
+//The sum of even numbers between 0 and 100 is: 2550
