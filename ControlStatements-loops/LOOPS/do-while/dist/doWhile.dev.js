09199d0512056d6f59173a4ed888a11192d21246
CHEGEBB <chegephil24@gmail.com>
2024-03-09 10:43:15 +0300

    update

---
+++ ControlStatements-loops/LOOPS/do-while/dist/doWhile.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//The do ... while statement is a loop in javascript and most programming languages in which a given piece of code is executed atleast onec before the
+//condition is checked
+//syntax
+//do{
+//statements to execute
+//}while(Condition/expression)
+//example:rolling a die
+var rollCount = 0;
+var isSixRolled = false;
+
+do {
+  // Simulate rolling the die (generate a random number between 1 and 6)
+  var dieResult = Math.floor(Math.random() * 6) + 1; // Increment the roll count
+
+  rollCount++; // Check if the rolled number is 6
+
+  if (dieResult === 6) {
+    // If a six is rolled, set isSixRolled to true
+    isSixRolled = true;
+  }
+} while (!isSixRolled);
+
+alert("It took " + rollCount + " rolls to roll a six.");
