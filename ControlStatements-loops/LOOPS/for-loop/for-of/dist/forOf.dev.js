a5ec10bc29f8fbe64361dc13d921718195f08be6
CHEGEBB <chegephil24@gmail.com>
2024-03-09 19:56:13 +0300

    for of

--- ControlStatements-loops/LOOPS/for-loop/for-of/dist/forOf.dev.js
+++ ControlStatements-loops/LOOPS/for-loop/for-of/dist/forOf.dev.js
@@ -1,3 +1,16 @@
+"use strict";
+
 //in the for ...of loop we dont specify any conditions or number of iterations
 //it is used to iterate over the elements of an array or the properties of an object
-"use strict";
+//syntax
+//for (variable declaration of array or object) { //code block to be executed}
+//example
+var arr = [1, 2, 3, 4, 5];
+var sum = 0;
+
+for (var _i = 0, _arr = arr; _i < _arr.length; _i++) {
+  var val = _arr[_i];
+  sum += val;
+}
+
+; //sum = 15
--- ControlStatements-loops/LOOPS/for-loop/for-of/forOf.js
+++ ControlStatements-loops/LOOPS/for-loop/for-of/forOf.js
@@ -1,2 +1,10 @@
 //in the for ...of loop we dont specify any conditions or number of iterations
 //it is used to iterate over the elements of an array or the properties of an object
+//syntax
+//for (variable declaration of array or object) { //code block to be executed}
+//example
+let arr = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let val of arr) {
+  sum += val;
+};//sum = 15
