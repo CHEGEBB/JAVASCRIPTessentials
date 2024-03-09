0889958251ab49f0a5727ef9b7b8cda85c98d6e3
CHEGEBB <chegephil24@gmail.com>
2024-03-09 20:02:59 +0300

    update

--- ControlStatements-loops/LOOPS/for-loop/for-of/dist/forOf.dev.js
+++ ControlStatements-loops/LOOPS/for-loop/for-of/dist/forOf.dev.js
@@ -14,3 +14,35 @@ for (var _i = 0, _arr = arr; _i < _arr.length; _i++) {
 }
 
 ; //sum = 15
+//example2
+
+var cities = [{
+  name: "New York",
+  population: 18.65e6
+}, {
+  name: "cairo",
+  population: 18.82e6
+}, {
+  name: "London",
+  population: 8.982e6
+}, {
+  name: "Paris",
+  population: 2.141e6
+}, {
+  name: "Tokyo",
+  population: 13.51e6
+}, {
+  name: "Beijing",
+  population: 21.54e6
+}, {
+  name: "Mumbai",
+  population: 20.7e6
+}, {
+  name: "Lagos",
+  population: 21e6
+}];
+
+for (var _i2 = 0, _cities = cities; _i2 < _cities.length; _i2++) {
+  var city = _cities[_i2];
+  if (city.population > 10e6) console.log("".concat(city.name, " : ").concat(city.population));
+}
--- ControlStatements-loops/LOOPS/for-loop/for-of/forOf.js
+++ ControlStatements-loops/LOOPS/for-loop/for-of/forOf.js
@@ -8,3 +8,19 @@ let sum = 0;
 for (let val of arr) {
   sum += val;
 };//sum = 15
+
+//example2
+let cities  =[
+    {name : "New York", population :18.65e6},
+    {name : "cairo", population :18.82e6},
+    {name : "London", population :8.982e6},
+    {name : "Paris", population :2.141e6},
+    {name : "Tokyo", population :13.51e6},
+    {name : "Beijing", population :21.54e6},
+    {name : "Mumbai", population :20.7e6},
+    {name : "Lagos", population :21e6},
+];
+for (let city of cities) {
+    if (city.population > 10e6)
+    console.log(`${city.name} : ${city.population}`);
+}
