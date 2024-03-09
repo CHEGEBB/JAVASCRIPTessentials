19a51acf0e4b0dcc3db735e89677da4d317f9a74
CHEGEBB <chegephil24@gmail.com>
2024-03-09 20:33:40 +0300

    update

---
+++ ControlStatements-loops/quiz2/dist/quiz.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+for (i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}
+
+var first = prompt("What is the first number?");
+var last = prompt("What is the last number?");
---
+++ ControlStatements-loops/quiz2/quiz.js
@@ -0,0 +1,5 @@
+for (i=100; i>=0; i-=10) {
+    console.log(i);
+}
+let first = prompt("What is the first number?");
+let last = prompt("What is the last number?");
