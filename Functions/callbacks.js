e88c2223e5ebba8426ad5ba9ba7487213996b19e
CHEGEBB <chegephil24@gmail.com>
2024-03-13 12:38:28 +0300

    update

---
+++ Functions/callbacks.js
@@ -0,0 +1,21 @@
+//callbacks are functions that are passed as arguments to other functions
+//callbacks are not executed immediately
+//callbacks are executed at a later time
+//there are two types of callbacks: synchronous and asynchronous
+//synchronous callbacks are executed immediately
+//asynchronous callbacks are executed at a later time
+//callbacks are used to make sure that certain code doesn't execute until other code has already finished
+//syncronous callback example
+function greeting(name) {
+    console.log('Hello ' + name);
+}
+function processUserInput(callback) {
+    let name = 'John';
+    callback(name);
+}
+processUserInput(greeting);//Hello John
+//explanation of the above code
+//the processUserInput function takes a callback function as an argument
+//the processUserInput function then calls the callback function and passes a name to it
+//the greeting function is the callback function
+//the greeting function is passed to the processUserInput function
