5330b1821af129b5ec182f0a6477afcf7807d714
CHEGEBB <chegephil24@gmail.com>
2024-03-14 09:33:04 +0300

    update

---
+++ Errors,Exceptions and handling/errors.js
@@ -0,0 +1,12 @@
+//javascript jusst like any other language has errors and exceptions
+//An error is an object which is thrown when an exception occurs
+//the following are the types of errors in javascript
+//1. EvalError: An error has occurred in the eval() function
+//example
+try {
+    eval('alert("Hello")')
+}
+catch (e) {
+    console.log(e)
+}
+//2. RangeError: An error has occurred when a number is not in the correct range
