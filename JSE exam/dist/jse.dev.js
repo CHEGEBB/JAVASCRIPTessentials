0082f7b959ccd8feecf684b7f3dcc4527a7f2b9f
CHEGEBB <chegephil24@gmail.com>
2024-03-14 10:02:09 +0300

    update

--- JSE exam/dist/jse.dev.js
+++ JSE exam/dist/jse.dev.js
@@ -1,10 +1,15 @@
 "use strict";
 
-var x = 3e-3;
-console.log(x); //0.003
+var m = 3e-3;
+console.log(m); //0.003
 
 var msg1 = 'hello';
 var msg2 = msg1.slice(-1);
 console.log(msg2 ? msg2 : msg2 + msg1); //o
 
 var test = prompt("Run", "code"); //Run
+
+var x = false || true;
+var y = "true" && "false";
+var z = false && true;
+console.log("".concat(x, " ").concat(y, " ").concat(z));
--- JSE exam/jse.js
+++ JSE exam/jse.js
@@ -1,6 +1,10 @@
-let x = 3e-3;
-console.log(x);//0.003
+let m = 3e-3;
+console.log(m);//0.003
 let msg1 = 'hello';
 let msg2 = msg1.slice(-1);
 console.log(msg2 ? msg2 : msg2 + msg1);//o
 let test = prompt("Run", "code");//Run
+let x = false || true;
+let y = "true" && "false";
+let z = false && true;
+console.log(`${x} ${y} ${z}`);
