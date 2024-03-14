2755c640da53d1200c79ca941e6fe7b8931fa168
CHEGEBB <chegephil24@gmail.com>
2024-03-14 09:35:28 +0300

    update

--- Errors,Exceptions and handling/dist/errors.dev.js
+++ Errors,Exceptions and handling/dist/errors.dev.js
@@ -10,3 +10,18 @@ try {
 } catch (e) {
   console.log(e);
 } //2. RangeError: An error has occurred when a number is not in the correct range
+//example
+
+
+try {
+  var num = 1;
+  if (num > 10) throw "Number is too big";
+} catch (e) {
+  console.log(e);
+}
+
+; //Number is too big
+//3. ReferenceError: An error has occurred when an invalid reference is used
+//example
+
+"let x =10";
--- Errors,Exceptions and handling/errors.js
+++ Errors,Exceptions and handling/errors.js
@@ -10,3 +10,70 @@ catch (e) {
     console.log(e)
 }
 //2. RangeError: An error has occurred when a number is not in the correct range
+//example
+try {
+    var num = 1
+    if (num > 10) throw "Number is too big"
+}
+catch (e) {
+    console.log(e)
+};//Number is too big
+//3. ReferenceError: An error has occurred when an invalid reference is used
+//example
+"let x =10";
+try {
+    console.log(x)
+};
+catch (e) {
+    console.log(e)
+};//ReferenceError: x is not defined
+//4. SyntaxError: An error has occurred when a syntax error occurs while parsing code
+//example
+try {
+    eval("alert('Hello)")
+}
+catch (e) {
+    console.log(e)
+};//SyntaxError: Unexpected token )
+//5. TypeError: An error has occurred when a value is not of the expected type
+//example
+try {
+    var num = 10
+    num.toUpperCase()
+}
+catch (e) {
+    console.log(e)
+}
+//6. URIError: An error has occurred when encodeURI() or decodeURI() are passed invalid parameters
+//example
+try {
+    decodeURI("%%%")
+}
+catch (e) {
+    console.log(e)
+}
+//7. InternalError: An error has occurred which is caused by an internal error in the JavaScript engine
+//example
+try {
+    throw new InternalError('An internal error occurred')
+}
+catch (e) {
+    console.log(e)
+}
+//8. Error: An error has occurred which is the generic error type and the base type for all other error types
+//example
+try {
+    throw new Error('An error occurred')
+}
+catch (e) {
+    console.log(e)
+}
+//9. AggregateError: An error that is the base type for all errors that occur for multiple asynchronous operations
+//example
+try {
+    throw new AggregateError('An error occurred')
+}
+catch (e) {
+    console.log(e)
+}
+//and many more
