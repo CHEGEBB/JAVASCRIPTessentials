68f57450984f8c9f2b2e807a1ae94f7dbbdaa21f
CHEGEBB <chegephil24@gmail.com>
2024-03-14 13:00:00 +0300

    update

--- Event_Listeners/dist/script.dev.js
+++ Event_Listeners/dist/script.dev.js
@@ -1,8 +1,19 @@
 "use strict";
 
-var grandparent = document.querySelector('.grandparent');
-var parent = document.querySelector('.parent');
-var child = document.querySelector('.child');
-grandparent.addEventListener('click', function (e) {
-  console.log("Grandparent 1");
+document.addEventListener('DOMContentLoaded', function () {
+  var grandparent = document.querySelector('.grandparent');
+  var parent = document.querySelector('.parent');
+  var child = document.querySelector('.child');
+  grandparent.addEventListener('click', function (e) {
+    console.log("Grandparent 1");
+  });
+  parent.addEventListener('click', function (e) {
+    console.log("Parent1");
+  });
+  child.addEventListener('click', function (e) {
+    console.log("child1");
+  });
+  document.addEventListener('click', function (e) {
+    console.log("document1");
+  });
 });
--- Event_Listeners/script.js
+++ Event_Listeners/script.js
@@ -1,7 +1,20 @@
-const grandparent  = document.querySelector('.grandparent');
-const parent = document.querySelector('.parent');
-const child = document.querySelector('.child');
+document.addEventListener('DOMContentLoaded', function() {
+    const grandparent = document.querySelector('.grandparent');
+    const parent = document.querySelector('.parent');
+    const child = document.querySelector('.child');
 
-grandparent.addEventListener('click',e => {
-    console.log("Grandparent 1");
-})
+    grandparent.addEventListener('click', e => {
+        console.log("Grandparent 1");
+    });
+
+    parent.addEventListener('click', e =>{
+
+        console.log("Parent1");
+    });
+    child.addEventListener('click', e =>{
+        console.log("child1");
+    });
+    document.addEventListener('click', e =>{
+        console.log("document1");
+    });
+})  
