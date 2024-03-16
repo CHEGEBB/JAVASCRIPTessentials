bfbfe58fe3f7a13ae36e074b41eeebff12e85f32
CHEGEBB <chegephil24@gmail.com>
2024-03-16 12:22:00 +0300

    update

--- ChangingHtmlWithJs/alt.js
+++ ChangingHtmlWithJs/alt.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("header").innerHTML = "See JavaScript changed me!!";
-    document.write(document.getElementById("second").innerHTML);
+    // document.write(document.getElementById("second").innerHTML);
+    document.getElementById("name").value = "Hello";
+
 });
--- ChangingHtmlWithJs/dist/alt.dev.js
+++ ChangingHtmlWithJs/dist/alt.dev.js
@@ -1,6 +1,7 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById("header").innerHTML = "See JavaScript changed me!!";
-  document.write(document.getElementById("second").innerHTML);
+  document.getElementById("header").innerHTML = "See JavaScript changed me!!"; // document.write(document.getElementById("second").innerHTML);
+
+  document.getElementById("name").value = "Hello";
 });
