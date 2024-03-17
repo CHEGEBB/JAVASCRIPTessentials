876a75f2b281d42ce2d2020c5e5079b1fbce6aa2
CHEGEBB <chegephil24@gmail.com>
2024-03-17 12:02:20 +0300

    update

--- ChangingHtmlWithJs/alt.js
+++ ChangingHtmlWithJs/alt.js
@@ -2,4 +2,6 @@ var books = document.querySelectorAll('#book-list li .name');
 Array.from(books).forEach(function(book){
     book.textContent += '(book title)';
     //to append text to the end of the text content we use += operator 
+
 });
+const bookList = document
--- ChangingHtmlWithJs/dist/alt.dev.js
+++ ChangingHtmlWithJs/dist/alt.dev.js
@@ -4,3 +4,4 @@ var books = document.querySelectorAll('#book-list li .name');
 Array.from(books).forEach(function (book) {
   book.textContent += '(book title)'; //to append text to the end of the text content we use += operator 
 });
+var bookList = document;
