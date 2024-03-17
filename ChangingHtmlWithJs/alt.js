3cd72fbe3ccac04e0db5f6d89927aed3e27591be
CHEGEBB <chegephil24@gmail.com>
2024-03-17 12:17:56 +0300

    update

--- ChangingHtmlWithJs/alt.js
+++ ChangingHtmlWithJs/alt.js
@@ -4,4 +4,6 @@ Array.from(books).forEach(function(book){
     //to append text to the end of the text content we use += operator 
 
 });
-const bookList = document
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML+= '<p>See This is how we add html..</p>';
