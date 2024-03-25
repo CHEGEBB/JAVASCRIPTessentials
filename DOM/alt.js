a04c7850af825e62f15013f98c1e733b5e8c30a9
CHEGEBB <chegephil24@gmail.com>
2024-03-25 11:47:38 +0300

    update

--- DOM/alt.js
+++ DOM/alt.js
@@ -1,9 +1,9 @@
 var books = document.querySelectorAll('#book-list li .name');
-Array.from(books).forEach(function(book){
+Array.from(books).forEach(function(book) {
     book.textContent += '(book title)';
     //to append text to the end of the text content we use += operator 
 
 });
 const bookList = document.querySelector('#book-list');
 // bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML+= '<p>See This is how we add html..</p>';
+bookList.innerHTML += '<p>See This is how we add html..</p>';
