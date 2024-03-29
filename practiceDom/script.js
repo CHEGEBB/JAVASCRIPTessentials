2ead17e78629f437179e5bc6d2bc951899e53f66
CHEGEBB <chegephil24@gmail.com>
2024-03-29 12:33:57 +0300

    js

---
+++ practiceDom/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const vehicleListing = document.querySelector('.vehicle-listing');
+    const detailsVehicle = document.querySelector('.details')
+    const card1 = document.querySelector('.card1')
+    const card1 = document.querySelector('.card1')
+    const card1 = document.querySelector('.card1')
+    const card1 = document.querySelector('.card1')
+
+});
