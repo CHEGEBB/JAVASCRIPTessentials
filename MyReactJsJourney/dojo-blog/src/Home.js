52f3669e9f7c5fece180166c79aca4dfbe7f3554
CHEGEBB <chegephil24@gmail.com>
2024-04-05 12:20:01 +0300

    react react

--- MyReactJsJourney/dojo-blog/src/Home.js
+++ MyReactJsJourney/dojo-blog/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
@@ -9,12 +10,7 @@ const Home = () => {
     return ( 
         <div className="home">
         <div className="blog-preview">
-        {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+       <BlogList blogs={blogs}  title="All Blogs"/>
         </div>
 
         </div>
