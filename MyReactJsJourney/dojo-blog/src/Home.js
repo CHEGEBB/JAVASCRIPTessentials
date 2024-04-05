458517659f775fc589755afe6872533c8bc4f138
CHEGEBB <chegephil24@gmail.com>
2024-04-05 11:35:21 +0300

    update

--- MyReactJsJourney/dojo-blog/src/Home.js
+++ MyReactJsJourney/dojo-blog/src/Home.js
@@ -1,18 +1,14 @@
 import { useState } from "react";
 const Home = () => {
-
-    const [name ,setName] =useState('mario');
-    const [age,setAge]=useState(20);
-    const handleClick = () =>{
-        setName('Brian');
-        setAge(19);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi ', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
+
         </div>
      );
 }
