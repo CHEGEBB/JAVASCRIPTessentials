4838f5fa990c374070f2a98fa181b49d64e01602
CHEGEBB <chegephil24@gmail.com>
2024-04-05 12:20:09 +0300

    react

---
+++ MyReactJsJourney/dojo-blog/src/BlogList.js
@@ -0,0 +1,16 @@
+const BlogList = (props) => {
+    const blogs =props.blogs;
+    const title =props.title;
+    return ( 
+        <div className="blog-list">
+             {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList; 
