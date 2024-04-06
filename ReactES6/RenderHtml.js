bc5a23bdf99f1fbb601bf8499f5f341c93bf7288
CHEGEBB <chegephil24@gmail.com>
2024-04-06 11:41:42 +0300

    update

---
+++ ReactES6/RenderHtml.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(myelement);
