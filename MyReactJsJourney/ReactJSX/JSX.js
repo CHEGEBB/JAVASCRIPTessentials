6a1dc5cb62912492306f2f66a5aebf0696db8792
CHEGEBB <chegephil24@gmail.com>
2024-04-06 12:13:04 +0300

    update

--- MyReactJsJourney/ReactJSX/JSX.js
+++ MyReactJsJourney/ReactJSX/JSX.js
@@ -5,3 +5,29 @@
 // JSX makes it easier to write and add HTML in React.
 
 
+//example
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const myElement = <h1>I Love JSX!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
+
+// With JSX you can write expressions inside curly braces { }.
+
+// The expression can be a React variable, or property, or any other valid JavaScript expression. JSX will execute the expression and return the result:
+
+ import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const myElement = (
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
