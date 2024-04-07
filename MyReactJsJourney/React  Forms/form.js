3b88f3fc626f0920e93d6d052de4f1f3de8bf6ec
CHEGEBB <chegephil24@gmail.com>
2024-04-07 16:15:01 +0300

    update

--- MyReactJsJourney/React  Forms/form.js
+++ MyReactJsJourney/React  Forms/form.js
@@ -52,3 +52,33 @@ function MyForm() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MyForm />);
+
+//submitting the form we need to use the onSubmit event handler.
+//The onSubmit event handler is used to handle the form submission.
+import { useState } from "react";
+import ReactDOM from 'react-dom/client';
+
+function MyForm() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
