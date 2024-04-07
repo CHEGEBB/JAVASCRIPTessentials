21b91d1330d0a85701769872c78d530f67d2b074
CHEGEBB <chegephil24@gmail.com>
2024-04-07 13:43:48 +0300

    day 3 my react journey

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -28,10 +28,20 @@ function App() {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
       <hr />
-      {list.map(function(item) {
-        return <div>{item.title}</div>;
-      })}
-    </div>
+      {
+      list.map(function(item) {
+      return (
+      <div key={item.objectID}>
+        <span>
+        <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+      );
+  })}
+  </div>
   );
 }
 
