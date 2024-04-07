01e97595f64bac5242c30d3dac70b2e5a9dfacc7
CHEGEBB <chegephil24@gmail.com>
2024-04-07 14:15:29 +0300

    react

--- MyReactJsJourney/React Conditional Rendering/condition.js
+++ MyReactJsJourney/React Conditional Rendering/condition.js
@@ -23,3 +23,29 @@ function IsGoal(props){
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Goal isGoal={false} />);//THIS WILL RENDER MissedGoal COMPONENT
 // root.render(<Goal isGoal={true} />);//THIS WILL RENDER MadeGoal COMPONENT
+
+//2. Conditional Rendering using logical && operator
+//The && operator is used to render the component based on the condition.
+//If the condition is true then the component is rendered otherwise it returns null.
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Garage(props) {
+  const cars = props.cars;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 &&
+        <h2>
+          You have {cars.length} cars in your garage.
+        </h2>
+      }
+    </>
+  );
+}
+
+const cars = ['Ford', 'BMW', 'Audi'];
+root.render(<Garage cars={cars} />);
+// If cars.length > 0 is equates to true, the expression after && will render.
+//If we empty the cars array, the expression after && will not render.
+
