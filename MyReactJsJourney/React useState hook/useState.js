99d68912c126613199d1948cf22d98ecd17ac6fa
CHEGEBB <chegephil24@gmail.com>
2024-04-07 17:23:46 +0300

    update

--- MyReactJsJourney/React useState hook/useState.js
+++ MyReactJsJourney/React useState hook/useState.js
@@ -27,3 +27,27 @@ function Car() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Car />);
+//the above can be simplified using a single object to store the state
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function Car() {
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+  });
+  
+  return (
+    <>
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car />);
