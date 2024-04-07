2c4a9846f4de47040f27b94d1c461f27c860956a
CHEGEBB <chegephil24@gmail.com>
2024-04-07 09:41:33 +0300

    react

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+const title = 'React';
 
 function App() {
-  // Define state variable for the counter
-  const [count, setCount] = useState(0);
+    return (
+      <div>
+          <h1> Hello {title} </h1>
+          <label htmlFor="search">Search: </label>
+          <input id="search" type="text" />
+          <button style={{backgroundColor:'aqua',borderRadius:'10px',width:'100px',height:'40px',border:'0',margin:'20px auto'}}>Search</button>
+      </div>
 
-  // Function to increment the counter
-  const incrementCounter = () => {
-    // Update the count state by incrementing its current value
-    setCount(count + 1);
-  };
-
-  return (
-    <div className="App">
-      <h1>Button Click Counter</h1>
-      <p>Count: {count}</p>
-      {/* Button to increment the counter */}
-      <button onClick={incrementCounter}>Increment</button>
-    </div>
-  );
+    );
 }
 
 export default App;
