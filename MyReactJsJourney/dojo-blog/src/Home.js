938d7a4741c758fa201f2fba4afcaaaa362b81c7
CHEGEBB <chegephil24@gmail.com>
2024-04-08 11:02:11 +0300

    update

--- MyReactJsJourney/dojo-blog/src/Home.js
+++ MyReactJsJourney/dojo-blog/src/Home.js
@@ -20,8 +20,7 @@ const Home = () => {
     return ( 
         <div className="home">
         <div className="blog-preview">
-       <BlogList blogs={blogs}  title="All Blogs." handleDelete={handleDelete}/>
-       <BlogList blogs={blogs.filter((blog) => blog.author ==='mario') }  title="Mario's Blogs."/>
+       {<BlogList blogs={blogs}  title="All Blogs." handleDelete={handleDelete}/>}
         </div>
 
         </div>
