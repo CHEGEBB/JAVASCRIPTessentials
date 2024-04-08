520c85b6fadd9287c9d08e80cd054ce9f0ab476c
CHEGEBB <chegephil24@gmail.com>
2024-04-08 10:22:41 +0300

    update

--- MyReactJsJourney/dojo-blog/src/Home.js
+++ MyReactJsJourney/dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -11,6 +11,10 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(blogs);
+    });
     
     return ( 
         <div className="home">
