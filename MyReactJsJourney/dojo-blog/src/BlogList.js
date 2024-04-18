7d4efd38b47d35c99c25a65130ce25743110c466
CHEGEBB <chegephil24@gmail.com>
2024-04-18 09:35:00 +0300

    ts

--- MyReactJsJourney/dojo-blog/src/BlogList.js
+++ MyReactJsJourney/dojo-blog/src/BlogList.js
@@ -1,7 +1,6 @@
 const BlogList = (props) => {
     const blogs =props.blogs;
     const title =props.title;
-    const handleDelete =props.handleDelete;
     return ( 
         <div className="blog-list">
         <h2>{title}</h2>
@@ -9,7 +8,6 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete Blog</button>
                 </div>
             ))}
         </div>
