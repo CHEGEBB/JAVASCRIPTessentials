1bed2f4f2fec1cda32818f3f9e042209c9d31002
CHEGEBB <chegephil24@gmail.com>
2024-04-18 09:34:54 +0300

    ts

---
+++ TYPESCRIPTEssentials/Typescript union types/union.ts
@@ -0,0 +1,11 @@
+//Union types are used when a value can be more than a single type.
+//Union types are defined by using a pipe (|) between the types.
+//Union types are used to define a variable with multiple types.
+//Using the | we are saying our parameter is a string or number
+//example 
+function printStatusCode(code: string | number) {
+  console.log(`My status code is ${code}.`)
+}
+
+printStatusCode(404);
+printStatusCode('404');
