ad5e642b61f61babf9442eb0b913452841dfc4b6
CHEGEBB <chegephil24@gmail.com>
2024-04-20 10:39:11 +0300

    ts

--- TYPESCRIPTEssentials/TypeScript Classes/classes.ts
+++ TYPESCRIPTEssentials/TypeScript Classes/classes.ts
@@ -119,4 +119,27 @@ interface Shape {
   const mySq = new Square(20);
   
   console.log(mySq.toString());
+
+//   Abstract Classes
+// Classes can be written in a way that allows them to be used as a base class for other classes without having to implement all the members. This is done by using the abstract keyword. Members that are left unimplemented also use the abstract keyword.
+abstract class Polygon {
+    public abstract getArea(): number;
   
+    public toString(): string {
+      return `Polygon[area=${this.getArea()}]`;
+    }
+  }
+  
+  class Rectangle extends Polygon {
+    public constructor(protected readonly width: number, protected readonly height: number) {
+      super();
+    }
+  
+    public getArea(): number {
+      return this.width * this.height;
+    }
+  }
+  
+  const myRect = new Rectangle(10,20);
+  
+  console.log(myRect.getArea());
