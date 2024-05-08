4b7a5c8914191296d2ee9ae9804ec464ff81e999
CHEGEBB <chegephil24@gmail.com>
2024-05-08 11:22:04 +0300

    stream

--- NODEJS/stream.js
+++ NODEJS/stream.js
@@ -1,1 +1,8 @@
 const fs = require('fs');
+
+const readstream = fs.createReadStream('./Modules/docs/doc3.txt', {encoding: 'utf8'});
+
+readstream.on('data', (chunk) =>{
+    console.log('-----------------NEW CHUNK-----------------');
+    console.log(chunk);
+})
