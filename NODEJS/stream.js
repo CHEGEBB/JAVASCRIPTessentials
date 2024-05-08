907d9903cad587786336c9cefdd3a6373d83d35c
CHEGEBB <chegephil24@gmail.com>
2024-05-08 11:24:46 +0300

    write stream

--- NODEJS/stream.js
+++ NODEJS/stream.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
 const readstream = fs.createReadStream('./Modules/docs/doc3.txt', {encoding: 'utf8'});
+const writestream = fs.createWriteStream('./Modules/docs/doc4.txt');
 
 readstream.on('data', (chunk) =>{
     console.log('-----------------NEW CHUNK-----------------');
     console.log(chunk);
+    writestream.write('\nNEW CHUNK\n');
+    writestream.write(chunk);
 })
