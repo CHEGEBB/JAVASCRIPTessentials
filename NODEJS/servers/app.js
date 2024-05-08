7bf065ad1308ed7a8e36b05c98cf4c204ede1bd4
CHEGEBB <chegephil24@gmail.com>
2024-05-08 14:26:06 +0300

    ejs

--- NODEJS/servers/app.js
+++ NODEJS/servers/app.js
@@ -10,16 +10,16 @@ app.listen(3000);
 
 app.get('/', (req, res) =>{
     // res.send('<p>Home Page</p>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 })
 
 app.get('/about', (req, res) =>{
     // res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
 })
 
 app.get('/contact', (req, res) =>{
-    res.sendFile('./views/contact.html', {root : __dirname});
+    res.render('contact');
 })
 
 // redirects
@@ -30,5 +30,5 @@ app.get('/about-us', (req, res)=> {
 
 // 404 page
 app.use((req, res) =>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 })
