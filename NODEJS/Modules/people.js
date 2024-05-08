180864d6a46289e37cebf44339a3c5e569cb24ea
CHEGEBB <chegephil24@gmail.com>
2024-05-08 10:21:40 +0300

    imports

--- NODEJS/Modules/people.js
+++ NODEJS/Modules/people.js
@@ -1,3 +1,9 @@
 
 const people = ['Brian', 'Sarah', 'John', 'Jane'];
+const countries = ['USA', 'Canada', 'Mexico', 'Brazil'];
 console.log(people);
+
+module.exports ={
+    people: people,
+    countries: countries
+};
