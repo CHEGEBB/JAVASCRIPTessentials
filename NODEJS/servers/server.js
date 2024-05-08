98ffdd23ac1590de4134a581efe6c32f2cea7a74
CHEGEBB <chegephil24@gmail.com>
2024-05-08 12:32:58 +0300

    res

--- NODEJS/servers/server.js
+++ NODEJS/servers/server.js
@@ -4,12 +4,25 @@ const fs = require('fs');
 const server = http.createServer((req,res) => {
     console.log(req.url, req.method);
 
-    // response header
+   
+    let path = './views/';
+    switch(req.url){
+        case '/' :
+            path += 'index.html';
+            break;
+        case '/about' :
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
     // res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Type', 'text/html');
 
     //send an html file
-    fs.readFile('./views/index.html', (err, data) =>{
+    fs.readFile(path, (err, data) =>{
         if(err){
             console.log(err);
             res.end();
