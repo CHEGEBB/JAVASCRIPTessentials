df5b30e024900b24f17be5fe345a276247267476
CHEGEBB <chegephil24@gmail.com>
2024-05-08 10:24:55 +0300

    destructuring

--- NODEJS/Modules/modules.js
+++ NODEJS/Modules/modules.js
@@ -2,5 +2,8 @@
 const need = require('./people');
 // we can use destructuring to get the values
 const {people} = require('./people');
+const {countries} = require('./people');
+
 
 console.log(people);
+console.log(countries);
