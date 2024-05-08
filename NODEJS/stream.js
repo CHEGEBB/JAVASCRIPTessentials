f0435cdb8bd467372ffc0e3dfa049476ff82d900
CHEGEBB <chegephil24@gmail.com>
2024-05-08 11:27:58 +0300

    piping

--- NODEJS/stream.js
+++ NODEJS/stream.js
@@ -3,9 +3,12 @@ const fs = require('fs');
 const readstream = fs.createReadStream('./Modules/docs/doc3.txt', {encoding: 'utf8'});
 const writestream = fs.createWriteStream('./Modules/docs/doc4.txt');
 
-readstream.on('data', (chunk) =>{
-    console.log('-----------------NEW CHUNK-----------------');
-    console.log(chunk);
-    writestream.write('\nNEW CHUNK\n');
-    writestream.write(chunk);
-})
+// readstream.on('data', (chunk) =>{
+//     console.log('-----------------NEW CHUNK-----------------');
+//     console.log(chunk);
+//     writestream.write('\nNEW CHUNK\n');
+//     writestream.write(chunk);
+// })
+
+// piping
+readstream.pipe(writestream);
