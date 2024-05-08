de073be223146e233275a887460b6f7a3744d527
CHEGEBB <chegephil24@gmail.com>
2024-05-08 12:46:45 +0300

    redirect

--- NODEJS/servers/server.js
+++ NODEJS/servers/server.js
@@ -1,41 +1,45 @@
-const http = require ('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method);
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
 
-   
-    let path = './views/';
-    switch(req.url){
-        case '/' :
-            path += 'index.html';
-            res.statusCode = 200;
-            break;
-        case '/about' :
-            path += 'about.html';
-            res.statusCode = 200;
-            break;
-        default:
-            path += '404.html';
-            res.statusCode = 404;
-            break;
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+        res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  // res.setHeader('Content-Type', 'text/plain');
+  res.setHeader("Content-Type", "text/html");
+
+  //send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
     }
-
-    // res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Type', 'text/html');
-
-    //send an html file
-    fs.readFile(path, (err, data) =>{
-        if(err){
-            console.log(err);
-            res.end();
-        } else {
-            // res.write(data);
-            res.end(data);
-        }
-    })
+  });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000');
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
 });
