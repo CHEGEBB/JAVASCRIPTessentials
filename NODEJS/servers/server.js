b314f1f7265fd03a8a7c7aba290d1e2fcf261948
CHEGEBB <chegephil24@gmail.com>
2024-05-08 12:02:22 +0300

    scss

--- NODEJS/servers/server.js
+++ NODEJS/servers/server.js
@@ -2,6 +2,10 @@ const http = require ('http');
 
 const server = http.createServer((req,res) => {
     console.log(req.url, req.method);
+
+    // response header
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello, world!');
 });
 
 server.listen(3000, 'localhost', () => {
