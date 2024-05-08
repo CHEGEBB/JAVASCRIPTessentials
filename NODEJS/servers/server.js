be78fb75e8c73c96a58d0dc1f224cedcdc011cac
CHEGEBB <chegephil24@gmail.com>
2024-05-08 11:49:23 +0300

    server

---
+++ NODEJS/servers/server.js
@@ -0,0 +1,9 @@
+const http = require ('http');
+
+const server = http.createServer((req,res) => {
+    console.log('request made');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000');
+});
