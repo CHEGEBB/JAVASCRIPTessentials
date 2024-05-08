ba580e158c20e1416be8d258807685fe704030e6
CHEGEBB <chegephil24@gmail.com>
2024-05-08 10:41:34 +0300

    write

--- NODEJS/Modules/File.js
+++ NODEJS/Modules/File.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
 // reading files
-fs.readFile('./docs/doc.txt', (err, data) =>{
-    if(err){
-        console.log(err);
-    }
-    console.log(data.toString());
-});
+// fs.readFile('./docs/doc.txt', (err, data) =>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+// writing files
+fs.writeFile('./docs/doc.txt', 'Hello, Again', () => {
+    console.log('file was written');
+})    
