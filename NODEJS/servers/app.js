95dddd4ba25751f6d8c415f63df9cde4d0cadbb0
CHEGEBB <chegephil24@gmail.com>
2024-05-08 13:45:35 +0300

    express routes

--- NODEJS/servers/app.js
+++ NODEJS/servers/app.js
@@ -8,3 +8,11 @@ app.listen(3000);
 app.get('/', (req, res) =>{
     res.send('<p>Home Page</p>');
 })
+
+app.get('/about', (req, res) =>{
+    res.send('<p>About Page</p>');
+})
+
+app.get('/contact', (req, res) =>{
+    res.send('<p>contact Page</p>');
+})
