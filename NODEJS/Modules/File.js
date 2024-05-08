8d5fb3d9653b333585b83af0897979adcc6739c4
CHEGEBB <chegephil24@gmail.com>
2024-05-08 10:36:51 +0300

    readfile

--- NODEJS/Modules/File.js
+++ NODEJS/Modules/File.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
 
 // reading files
-fs.readFile('./docs/doc.txt');
+fs.readFile('./docs/doc.txt', (err, data) =>{
+    if(err){
+        console.log(err);
+    }
+    console.log(data);
+});
