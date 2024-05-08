f6d40b47bc64b93de981c1b91eebbf5bb74e1742
CHEGEBB <chegephil24@gmail.com>
2024-05-08 11:56:44 +0300

    requests

--- NODEJS/servers/server.js
+++ NODEJS/servers/server.js
@@ -1,7 +1,7 @@
 const http = require ('http');
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+    console.log(req.url, req.method);
 });
 
 server.listen(3000, 'localhost', () => {
