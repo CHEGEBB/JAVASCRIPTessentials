7bb73e2311c4f8dc9dedbb37409c3b6d91b3466f
CHEGEBB <chegephil24@gmail.com>
2024-05-10 11:01:21 +0300

    query

--- NODEJS/Modules/url/url.js
+++ NODEJS/Modules/url/url.js
@@ -12,3 +12,7 @@ const q =url.parse(adr , true);
 console.log(q.host); //returns 'localhost:8080'
 console.log(q.pathname); //returns '/default.htm'
 console.log(q.search); //returns '?year=2021&month=february'
+
+const qdata = q.query; //returns an object: { year: 2021, month: 'february' }
+console.log(qdata.month); 
+console.log(qdata);
