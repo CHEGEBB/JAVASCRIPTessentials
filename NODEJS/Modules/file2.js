40f39e64ebaecfe18b633195efb6610db7046888
CHEGEBB <chegephil24@gmail.com>
2024-05-10 10:42:47 +0300

    append

--- NODEJS/Modules/file2.js
+++ NODEJS/Modules/file2.js
@@ -1,11 +1,20 @@
 // The fs.appendFile() method appends specified content to a file. If the file does not exist, the file will be created:
 
 const fs = require('fs');
-
-fs.appendFile('./docs/newappend.txt', 'Hello Content', (err, data) => {
+if(fs.existsSync('./docs/newappend.txt')) {
+    fs.unlink('./docs/newappend.txt', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('File deleted');
+        }
+    });
+}else{
+fs.appendFile('./docs/newappend.txt', 'Hello Content', (err) => {
     if (err) {
         console.log(err);
     } else {
         console.log('File created and content added');
     }
 });
+}
