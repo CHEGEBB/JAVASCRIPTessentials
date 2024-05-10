4316177c41afaadc745d395c3e864f5790cc5bc3
CHEGEBB <chegephil24@gmail.com>
2024-05-10 10:56:07 +0300

    url

--- NODEJS/Modules/url/url.js
+++ NODEJS/Modules/url/url.js
@@ -1,3 +1,14 @@
 // The URL module splits up a web address into readable parts.
 
 // To include the URL module, use the require() method:
+
+const url = require('url');
+
+const adr = 'http://localhost:8080/default.htm?year=2021&month=february';
+// Parse the address:
+
+const q =url.parse(adr , true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2021&month=february'
