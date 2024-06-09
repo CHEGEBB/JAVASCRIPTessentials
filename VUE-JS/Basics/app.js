353e9d582c3c557a57c920d8cf05f1febd8dd480
CHEGEBB <chegephil24@gmail.com>
2024-06-09 12:51:50 +0300

    new

--- VUE-JS/Basics/app.js
+++ VUE-JS/Basics/app.js
@@ -11,7 +11,7 @@ const app =Vue.createApp ({
             this.title = 'Words of Radiance'
         }
         changeAuthor:{
-            this.author=""
+            this.author="me"
         }
     }
 })
