c8f8891090a3e6256a1f2bfa6fb0d0315211c4c8
CHEGEBB <chegephil24@gmail.com>
2024-06-19 17:28:36 +0300

    new

--- NEXTJS/next-blog/app/tickets/TicketsList.jsx
+++ NEXTJS/next-blog/app/tickets/TicketsList.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
-export default async function TicketsList() {
+async function getTickets() {
+  const res = await fetch('http://localhost:4000/tickets')
+  return res.json()
+}
+
+export default async function TicketList() {
+    const tickets = await getTickets()
   return (
-    <div>TicketsList</div>
+    <>
+    {tickets.map((ticket)=>((
+
+        <div className="card" key={ticket.id}>
+        <h3>{ticket.title}</h3>
+        <p>{ticket.body.slice(0, 200)}...</p>
+        <div className={`pill ${ticket.priority}`}>
+            {ticket.priority}priority
+        </div>
+
+        </div>
+        
+    )))}
+    {tickets.length === 0 && (
+        <p className='text-center'>There are no </p>
+    )}
+
+    </>
   )
 }
