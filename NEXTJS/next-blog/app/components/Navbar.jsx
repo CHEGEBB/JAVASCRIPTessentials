873042285d85bd5255ced6c9c36350c40f150646
CHEGEBB <chegephil24@gmail.com>
2024-06-19 17:01:57 +0300

    added logo

--- NEXTJS/next-blog/app/components/Navbar.jsx
+++ NEXTJS/next-blog/app/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
+import Logo from './dojo-logo.png'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
     <nav>
+
+    <Image
+    src={Logo}
+    alt='logo'
+     />
     <Link href="/">Dashboard</Link>
     <Link href="/tickets">tickets</Link>
   </nav>
