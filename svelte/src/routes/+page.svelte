caab1861c4a1744a037b2b957f9da7e4c6bb464c
CHEGEBB <chegephil24@gmail.com>
2024-11-09 15:19:41 +0300

    Add dynamic greeting message in App component

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let user ="Brian Chege"
+</script>
+<h2 >
+    Hello, my name is {user}!
+    <br>
+    I'm a software engineer specializing in full-stack development.
+    <p>
+        I recently graduated from the University of Texas at Austin with a degree in Computer Science. I'm passionate about learning new technologies and collaborating with other developers to create innovative solutions.
+    </p>
+</h2>
+<style>
+   
+    h2 {
+        color: white;
+        font-size: 24px;
+        text-align: center;
+        position: relative;
+        margin-top: 50px;
+        animation: fade-in 2s ease-in-out;
+        z-index: 100;
+
+    }
+    p {
+        font-size: 18px;
+    }
+    /* Add your CSS styles here */
+</style>
