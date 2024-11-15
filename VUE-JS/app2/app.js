08df27a790400181f2e8295f1ef792f92feffa5c
CHEGEBB <chegephil24@gmail.com>
2024-11-15 15:37:12 +0300

    Refactor Vue app to add toggle functionality for showing and hiding books

--- VUE-JS/app2/app.js
+++ VUE-JS/app2/app.js
@@ -2,10 +2,16 @@
 const app = Vue.createApp({
     data () {
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
             
+        }
+    },
+    methods : {
+        toggleShowBooks (){
+            this.showBooks =!this.showBooks
         }
     }
 })
