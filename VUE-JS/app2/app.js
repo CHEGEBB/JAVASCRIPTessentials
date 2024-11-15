4ba7259817839576741913a47efb1ba8dfba03f0
CHEGEBB <chegephil24@gmail.com>
2024-11-15 17:41:20 +0300

    Refactor Vue app to update book display with a list of books and add toggle functionality for showing and hiding books

--- VUE-JS/app2/app.js
+++ VUE-JS/app2/app.js
@@ -2,11 +2,12 @@
 const app = Vue.createApp({
     data () {
         return {
+            url : 'https://chegebb.github.io/My-portfolio-website/',
             showBooks: true,
             books : [
-                {title: 'The Catcher in the Rye', author: 'J.D. Salinger', pages: 276},
-                {title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 297},
-                {title: '1984', author: 'George Orwell', pages: 328},
+                {title: 'The Catcher in the Rye', author: 'J.D. Salinger', pages: 276, img : './assets/1.jpeg',isFav: true,},
+                {title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 297 , img : './assets/2.jpeg',isFav: false,},
+                {title: '1984', author: 'George Orwell', pages: 328 , img : './assets/3.jpeg',isFav: true,},
             ],
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
@@ -27,6 +28,9 @@ const app = Vue.createApp({
             this.x = e.offsetX
             this.y = e.offsetY
 
+        },
+        toggleIsFav(book){
+            book.isFav =!book.isFav
         }
     }
 })
