b44aa46bb2ee346394b6773686057a5c28251b53
CHEGEBB <chegephil24@gmail.com>
2024-11-15 16:26:46 +0300

    Refactor Vue app to add toggle functionality for showing and hiding books

--- VUE-JS/app2/app.js
+++ VUE-JS/app2/app.js
@@ -3,15 +3,30 @@ const app = Vue.createApp({
     data () {
         return {
             showBooks: true,
+            books : [
+                {title: 'The Catcher in the Rye', author: 'J.D. Salinger', pages: 276},
+                {title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 297},
+                {title: '1984', author: 'George Orwell', pages: 328},
+            ],
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
             
         }
     },
     methods : {
         toggleShowBooks (){
             this.showBooks =!this.showBooks
+        },
+        handleEvent(e){
+            console.log('event', e.type)
+        },
+        handleMouseMove (e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+
         }
     }
 })
