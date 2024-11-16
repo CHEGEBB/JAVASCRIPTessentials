dfc714aacb8281638dfe13fa3a26b36b23560bff
CHEGEBB <chegephil24@gmail.com>
2024-11-16 14:38:19 +0300

    Refactor Vue app to update main heading style

--- VUE-JS/modal-project/src/App.vue
+++ VUE-JS/modal-project/src/App.vue
@@ -1,5 +1,9 @@
 <template>
+  <div >
     <h1>{{title}}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">Click me</button>
+    </div>
  
 </template>
 
@@ -11,6 +15,13 @@ export default {
     return{
       title: 'My First Vue app'
   }
+  },
+  methods: {
+    handleClick (){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+    }
+
   }
 }
 </script>
@@ -23,5 +34,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  color: red;
+  font-size: 30px;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  transition: color 0.5s;
+  cursor: pointer;
+  border-bottom: 2px solid #ccc;
 }
 </style>
