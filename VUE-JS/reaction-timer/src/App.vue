dbb15a3c68fcf137f10bc2059ae2ec2a8f128f95
CHEGEBB <chegephil24@gmail.com>
2024-11-18 12:03:08 +0300

    Refactor Vue app to add start button and Block component

--- VUE-JS/reaction-timer/src/App.vue
+++ VUE-JS/reaction-timer/src/App.vue
@@ -1,12 +1,33 @@
 <template>
+  <div>
   <h1>Reaction Timer</h1>
+  <button @click="start">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
+</div>
 </template>
 
 <script>
+  import Block from './components/Block.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isPlaying :false,
+      delay:null,
+    };
+  },
   components: {
+    Block,
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random()*5000
+      this.isPlaying = true;
+      console.log(this.delay);
+   
+
+    }
   }
 }
 </script>
