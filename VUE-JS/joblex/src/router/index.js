e9c9e35a2a4b42c2485bd6f1a8da9497906c49ac
CHEGEBB <chegephil24@gmail.com>
2024-11-20 11:36:56 +0300

    added about

--- VUE-JS/joblex/src/router/index.js
+++ VUE-JS/joblex/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import About from '../views/AboutView.vue'
+
 
 const routes = [
   {
@@ -10,10 +12,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component:About,
   }
 ]
 
