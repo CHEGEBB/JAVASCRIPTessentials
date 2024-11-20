01b4d2917c89dc6c38421ed94ad2f529b326a52f
CHEGEBB <chegephil24@gmail.com>
2024-11-20 11:54:19 +0300

    Refactor AboutView.vue and HomeView.vue components to remove unnecessary code and add placeholder text

--- VUE-JS/joblex/src/views/AboutView.vue
+++ VUE-JS/joblex/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod suscipit tenetur quibusdam nobis numquam officia doloribus nostrum in possimus aut facilis, sed fugiat maxime ipsam velit repellat sapiente aliquid dolores totam at eos. Ratione sapiente quam, repellendus alias iure maiores pariatur. Rerum, quia repudiandae eveniet inventore obcaecati possimus eum, necessitatibus, animi delectus veritatis ducimus harum quidem! Repellendus voluptatum eius minus quia distinctio quae expedita corporis nostrum! Architecto alias ducimus recusandae quo quidem ipsam sed blanditiis repellat nihil beatae, quisquam perferendis corrupti rerum nobis, sit ex illo voluptatem modi deserunt cum repudiandae fugiat? Molestias eius voluptatem et dolor ea dolore vel?</p>
   </div>
 </template>
--- VUE-JS/joblex/src/views/HomeView.vue
+++ VUE-JS/joblex/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod suscipit tenetur quibusdam nobis numquam officia doloribus nostrum in possimus aut facilis, sed fugiat maxime ipsam velit repellat sapiente aliquid dolores totam at eos. Ratione sapiente quam, repellendus alias iure maiores pariatur. Rerum, quia repudiandae eveniet inventore obcaecati possimus eum, necessitatibus, animi delectus veritatis ducimus harum quidem! Repellendus voluptatum eius minus quia distinctio quae expedita corporis nostrum! Architecto alias ducimus recusandae quo quidem ipsam sed blanditiis repellat nihil beatae, quisquam perferendis corrupti rerum nobis, sit ex illo voluptatem modi deserunt cum repudiandae fugiat? Molestias eius voluptatem et dolor ea dolore vel?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod suscipit tenetur quibusdam nobis numquam officia doloribus nostrum in possimus aut facilis, sed fugiat maxime ipsam velit repellat sapiente aliquid dolores totam at eos. Ratione sapiente quam, repellendus alias iure maiores pariatur. Rerum, quia repudiandae eveniet inventore obcaecati possimus eum, necessitatibus, animi delectus veritatis ducimus harum quidem! Repellendus voluptatum eius minus quia distinctio quae expedita corporis nostrum! Architecto alias ducimus recusandae quo quidem ipsam sed blanditiis repellat nihil beatae, quisquam perferendis corrupti rerum nobis, sit ex illo voluptatem modi deserunt cum repudiandae fugiat? Molestias eius voluptatem et dolor ea dolore vel?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
