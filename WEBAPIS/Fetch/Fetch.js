42c2a9bed7350c79ccc37b7c3138fb28adc7fd52
CHEGEBB <chegephil24@gmail.com>
2024-11-26 19:32:16 +0300

    Refactor Fetch.js to add comments explaining the fetch API and how to handle responses and errors

---
+++ WEBAPIS/Fetch/Fetch.js
@@ -0,0 +1,16 @@
+/*What is fetch?
+fetch is a modern JavaScript API used to make network requests (like GET, POST, etc.) to a server and retrieve data.
+It's promise-based, meaning it returns a Promise that resolves when the request is complete.*/
+/*
+Making a GET Request
+
+A GET request is used to retrieve data from a server.
+fetch(url) initiates a request to the specified URL and returns a Promise.
+Handling the Response
+
+The response object contains the data from the server.
+Use .json() to parse JSON data from the response.
+Catching Errors
+
+Handle errors using .catch() to ensure your app doesn't crash
+ */
